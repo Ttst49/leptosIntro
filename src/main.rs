83b37bd80509cb80a1bc8f22e7f73466407e2b7c
thibaut.stachnick <thibautstachnick@gmail.com>
2024-05-09 15:18:19 +0200

    add a way to change color once we clicked, add a button that increment a counter

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,22 @@ use leptos::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! {<h1>Hello World</h1>})
+    mount_to_body(|| view! { <App />})
+}
+
+#[component]
+fn App()->impl IntoView{
+    let (count,set_count) = create_signal(0);
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n +=1)
+            }
+        class:red=move || count() % 2==1
+
+        >
+            "Click me: "
+            {count}
+        </button>
+    }
 }
